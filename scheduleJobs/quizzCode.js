8f0471326f6c2f642d78fc61afd78046a4878b03
Josu Vazquez <josu9513@gmail.com>
2022-03-12 17:38:39 +0100

    Fixture: Remove comments

--- scheduleJobs/quizzCode.js
+++ scheduleJobs/quizzCode.js
@@ -13,13 +13,3 @@ class QuizzCode {
 }
 
 module.exports = new QuizzCode();
-
-// export const createCode = () => {
-//     let todaysCode = '';
-//     for(let index = 0; index < 5; index++) {
-//         const randomNum = Math.floor(Math.random() * (9 - 0 + 1) + 0);
-//         todaysCode += randomNum;
-//     }
-//     const newItem = await Quizz.create({ code: todaysCode, date: new Date() });
-//     console.log(newItem);
-// };
